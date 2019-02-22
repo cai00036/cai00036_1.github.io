12086d70240cbbe0b758c4807cccf7931e4952a0
wei cai <cai00036@algonquinlive.com>
2019-02-21 19:26:05 -0500

    AddGulp Sass compiler

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+
+gulp.task('sass', function () {
+  return gulp
+    .src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+})
